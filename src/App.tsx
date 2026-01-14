ab2a1a8a2037158258bf54695df640429fbdc917
pintaigao <pintaigao@gmail.com>
2026-01-14 12:31:49 -0600

    Init the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {RouterProvider} from "react-router-dom";
+import routerConfig from "./router";
+
+function App() {
+  return (
+    <RouterProvider router={routerConfig}></RouterProvider>
+  )
+}
+
+export default App
